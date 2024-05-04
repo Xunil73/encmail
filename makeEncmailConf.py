2fc49a1e3e5c03a54f7b830859177fb4ccf11fd3
Harry Seiler <dj5my@ok.de>
2024-05-04 13:21:55 +0200

    solved layout error

--- makeEncmailConf.py
+++ makeEncmailConf.py
@@ -15,7 +15,7 @@ conffile='/home/harry/DATA/Entwicklung/git/encmail/encmail.conf'
 print("die Datei wird angelegt...\n")
 emailadress = input("Ihre Emailadresse: ")
 emailserver = input("Name des Mailservers: ")
-gpg_excludes = input("Schlagwörter die auf Schlüssel passen\ndie ausgeblendet werden sollen\n(kommagetrennte Tags):")
+gpg_excludes = input("Schlagwörter die auf Schlüssel passen\ndie ausgeblendet werden sollen\n(kommagetrennte Tags): ")
 text='# config-File fuer encmail\n# diese Datei wurde durch makeEncmailConf.py erstellt \
       \n# Editieren sie diese File nach eigenen Anforderungen oder loeschen sie sie\n \
       \n# und erzeugen eine neue Datei durch Ausführen von Encmail \
