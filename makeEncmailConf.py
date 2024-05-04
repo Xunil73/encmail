9342b9d9929303462327778d8818d5d43f086695
Harry Seiler <dj5my@ok.de>
2024-05-04 11:07:59 +0200

    keyring.set_password() works for email and gpg entry

--- makeEncmailConf.py
+++ makeEncmailConf.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+### TODO ###
+# rename keyring user 'gpg_posteo' to neutral name in ALL Files
+### TODO END ###
+
+import os
 import keyring
 
 
@@ -29,4 +34,35 @@ except:
   print("Fehler bei Dateioperation")
 finally:
   fobj.close()
+
+mail_passwd_ok=False
+while mail_passwd_ok != True:
+  mail_passwd = input("Passwort für den Emailaccount: ")
+  mail_ref_passwd = input("Password wiederholen: ")
+  if mail_passwd == mail_ref_passwd:
+    mail_passwd_ok=True
+    print("die Passwörter stimmen überein.\nPasswort wird im Keyring gespeichert.")
+  else:
+    print("die Passwörter stimmen nicht überein!")
+
+gpg_passwd_ok=False
+while gpg_passwd_ok != True:
+  gpg_passwd = input("Passwort für GPG-Key: ")
+  gpg_ref_passwd = input("Passwort wiederholen: ")
+  if gpg_passwd == gpg_ref_passwd:
+    gpg_passwd_ok=True
+    print("die Passwörter stimmen überein.\nPasswort wird im Keyring gespeichert.")
+  else:
+    print("die Passwörter stimmen nicht überein!")
+
+try:
+  keyring.set_password("email", emailadress, mail_passwd)
+  keyring.set_password("gpg_posteo", emailadress, gpg_passwd)
+except:
+  print("Fehler - Passwörter konnten nicht im Keyring gespeichert werden.")
+finally:
+  mail_passwd=None
+  mail_ref_passwd=None
+  gpg_passwd=None
+  gpg_ref_passwd=None
 
